739e0a06e7edbe9a094ec4427f58245fbe669ec1
shuhofest2022 <109235208+shuhofest2022@users.noreply.github.com>
2022-07-19 17:19:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
- /*----------HTML内へ記述する場合は、<script>〜</script>で、囲いましょう*/
 $(function() {
 　$('.Toggle').click(function() {
    $(this).toggleClass('active');
